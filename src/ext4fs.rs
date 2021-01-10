4b42fe77fd9fa9bc5ba1cb505e20fee4136157f0
Chris West (Faux) <git@goeswhere.com>
2021-01-10 12:42:25 +0000

    chore: cargo fmt --all

--- src/ext4fs.rs
+++ src/ext4fs.rs
@@ -2,8 +2,8 @@
 //
 //
 
+use std::ffi::OsString;
 use std::fs;
-use std::ffi::{OsString};
 
 use fuse_mt::*;
 
@@ -14,24 +14,22 @@ pub struct Ext4FS {
 //impl PassthroughFS {
 //}
 
-
 impl FilesystemMT for Ext4FS {
-
     //fn init(&self, _req: RequestInfo) -> ResultEmpty {
-        //println!("init");
+    //println!("init");
 
-        //let r = fs::File::open(&self.target).expect("openable file");
-        //let mut options = ext4::Options::default();
-        //options.checksums = ext4::Checksums::Enabled;
-        //let mut vol = ext4::SuperBlock::new_with_options(r, &options).expect("ext4 volume");
-        //let root = vol.root().expect("root");
-        //vol.walk(&root, "/", &mut |_, path, _, _| {
-            //println!("{}", path);
-            //Ok(true)
-        //})
-        //.expect("walk");
+    //let r = fs::File::open(&self.target).expect("openable file");
+    //let mut options = ext4::Options::default();
+    //options.checksums = ext4::Checksums::Enabled;
+    //let mut vol = ext4::SuperBlock::new_with_options(r, &options).expect("ext4 volume");
+    //let root = vol.root().expect("root");
+    //vol.walk(&root, "/", &mut |_, path, _, _| {
+    //println!("{}", path);
+    //Ok(true)
+    //})
+    //.expect("walk");
 
-        //Ok(())
+    //Ok(())
     //}
 
     fn init(&self, _req: RequestInfo) -> ResultEmpty {
@@ -41,7 +39,6 @@ impl FilesystemMT for Ext4FS {
         let mut superblock = ext4::SuperBlock::new(&mut block_device).unwrap();
         let target_inode_number = superblock.resolve_path("/").unwrap().inode;
         println!("{}", target_inode_number);
-
 
         Ok(())
     }
