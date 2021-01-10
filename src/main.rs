1bea93e9bce49ef36aa63dfa2df82ffafe586e14
Chris West (Faux) <git@goeswhere.com>
2021-01-10 12:42:15 +0000

    feat: use anyhow to unwind main()

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,32 @@
-extern crate ext4;
-
 use std::env;
 use std::ffi::{OsStr, OsString};
+
+use anyhow::Result;
 
 mod ext4fs;
 
 //fn main() {
-    //let r = fs::File::open(env::args().nth(1).expect("one argument")).expect("openable file");
-    //let mut options = ext4::Options::default();
-    //options.checksums = ext4::Checksums::Enabled;
-    //let mut vol = ext4::SuperBlock::new_with_options(r, &options).expect("ext4 volume");
-    //let root = vol.root().expect("root");
-    //vol.walk(&root, "/", &mut |_, path, _, _| {
-        //println!("{}", path);
-        //Ok(true)
-    //})
-    //.expect("walk");
+//let r = fs::File::open(env::args().nth(1).expect("one argument")).expect("openable file");
+//let mut options = ext4::Options::default();
+//options.checksums = ext4::Checksums::Enabled;
+//let mut vol = ext4::SuperBlock::new_with_options(r, &options).expect("ext4 volume");
+//let root = vol.root().expect("root");
+//vol.walk(&root, "/", &mut |_, path, _, _| {
+//println!("{}", path);
+//Ok(true)
+//})
+//.expect("walk");
 //}
 
-
-fn main() {
+fn start() -> Result<i32> {
     let args: Vec<OsString> = env::args_os().collect();
 
     if args.len() != 3 {
-        println!("usage: {} <target> <mountpoint>", &env::args().next().unwrap());
-        ::std::process::exit(-1);
+        println!(
+            "usage: {} <target> <mountpoint>",
+            &env::args().next().unwrap()
+        );
+        return Ok(2);
     }
 
     let filesystem = ext4fs::Ext4FS {
@@ -33,5 +35,11 @@ fn main() {
 
     let fuse_args: Vec<&OsStr> = vec![&OsStr::new("-o"), &OsStr::new("auto_unmount")];
 
-    fuse_mt::mount(fuse_mt::FuseMT::new(filesystem, 1), &args[2], &fuse_args).unwrap();
+    fuse_mt::mount(fuse_mt::FuseMT::new(filesystem, 1), &args[2], &fuse_args)?;
+
+    Ok(0)
+}
+
+fn main() -> Result<()> {
+    ::std::process::exit(start()?)
 }
