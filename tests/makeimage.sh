f2da8df9422be77b9cddf154a918e3455ac5671c
Aaron Ash <aaron.ash@gmail.com>
2021-01-16 21:48:14 +1000

    test(makeimage.sh): add some test txt files to the image

--- tests/makeimage.sh
+++ tests/makeimage.sh
@@ -3,5 +3,17 @@ dd if=/dev/zero bs=1M count=1000 > ext4fs.img
 # use loopback device and then mkfs.ext4 the "device"
 LOOPDEV=$(losetup --show --find ext4fs.img)
 mkfs -t ext4 "$LOOPDEV"
-losetup -d "$LOOPDEV"
 
+TMPDIR=$(mktemp -d)
+mount "$LOOPDEV" "$TMPDIR"
+
+echo "$TMPDIR"
+
+# Create some test files
+cd "$TMPDIR"
+uname -a > uname.txt
+iostat > iostat.txt
+cd -
+
+umount "$TMPDIR"
+losetup -d "$LOOPDEV"
