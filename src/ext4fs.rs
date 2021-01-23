532936c2ca4c2cfb313abc9a368ee61685b1651e
Aaron Ash <aaron.ash@gmail.com>
2021-01-23 22:10:08 +1000

    broken: Fails to build and is wrong in general
    I'm new to rust and don't understand how I can implement the
    FilesystemMT trait for Ext4FS if it's not allowed to mutate itself.

--- src/ext4fs.rs
+++ src/ext4fs.rs
@@ -53,12 +53,12 @@ impl Ext4FS {
 
 const ONLY_FH: u64 = 5318008;
 
-impl FilesystemMT for Ext4FS {
-    fn init(&self, _req: RequestInfo) -> ResultEmpty {
+impl FilesystemMT for &mut Ext4FS {
+    fn init(&mut self, _req: RequestInfo) -> ResultEmpty {
         Ok(())
     }
 
-    fn getattr(&self, _req: RequestInfo, path: &Path, _fh: Option<u64>) -> ResultEntry {
+    fn getattr(&mut self, _req: RequestInfo, path: &Path, _fh: Option<u64>) -> ResultEntry {
         println!("{:?}", path);
         if path != Path::new("/") {
             return Err(libc::ENOENT);
@@ -83,15 +83,31 @@ impl FilesystemMT for Ext4FS {
         ))
     }
 
-    fn opendir(&self, _req: RequestInfo, path: &Path, _flags: u32) -> ResultOpen {
-        if Path::new("/") == path {
-            Ok((ONLY_FH, 0))
-        } else {
-            Err(libc::ENOENT)
+    fn opendir(&mut self, _req: RequestInfo, path: &Path, _flags: u32) -> ResultOpen {
+        // let inode = self.superblock.resolve_path(path.to_str().unwrap()).unwrap().inode;
+
+        // match self.superblock.resolve_path(path.to_str().unwrap()) {
+        let result = self.superblock.resolve_path(path.to_str().unwrap());
+        match result {
+            Ok(dirent) => {
+                // TODO inode is not a filehandle (maybe?)
+                let filehandle = dirent.inode.into();
+                Ok((filehandle, 0))
+            }
+            Err(e) => {
+                println!("opendir() error: {:?}", e);
+                Err(libc::ENOENT)
+            }
         }
+
+        // if Path::new("/") == path {
+        //     Ok((ONLY_FH, 0))
+        // } else {
+        //     Err(libc::ENOENT)
+        // }
     }
 
-    fn readdir(&self, _req: RequestInfo, _path: &Path, fh: u64) -> ResultReaddir {
+    fn readdir(&mut self, _req: RequestInfo, _path: &Path, fh: u64) -> ResultReaddir {
         if ONLY_FH == fh {
             Ok(vec![])
         } else {
@@ -99,7 +115,7 @@ impl FilesystemMT for Ext4FS {
         }
     }
 
-    fn releasedir(&self, _req: RequestInfo, _path: &Path, fh: u64, _flags: u32) -> ResultEmpty {
+    fn releasedir(&mut self, _req: RequestInfo, _path: &Path, fh: u64, _flags: u32) -> ResultEmpty {
         if ONLY_FH == fh {
             Ok(())
         } else {
