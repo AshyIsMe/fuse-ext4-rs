e1b107c9254b7dda3c06e70527f5c4ff4b635667
Chris West (Faux) <git@goeswhere.com>
2021-01-29 07:52:46 +0000

    feat: return EROFS for write-y operations

--- src/ext4fs.rs
+++ src/ext4fs.rs
@@ -3,6 +3,7 @@
 //
 
 use std::convert::TryFrom;
+use std::ffi::OsStr;
 use std::ffi::OsString;
 use std::io;
 use std::path::Path;
@@ -13,6 +14,7 @@ use ext4::Enhanced;
 use ext4::FileType as ExtFileType;
 use fuse_mt::*;
 use positioned_io::ReadAt;
+use time::Timespec;
 
 pub trait BlockDevice: ReadAt + Send + Sync {}
 impl<T: ReadAt + Send + Sync> BlockDevice for T {}
@@ -67,6 +69,8 @@ impl FilesystemMT for Ext4FS {
         Ok(())
     }
 
+    fn destroy(&self, _req: RequestInfo) {}
+
     fn getattr(&self, _req: RequestInfo, path: &Path, _fh: Option<u64>) -> ResultEntry {
         println!("{:?}", path);
         if path != Path::new("/") {
@@ -90,6 +94,161 @@ impl FilesystemMT for Ext4FS {
                 flags: 0,
             },
         ))
+    }
+
+    fn chmod(&self, _req: RequestInfo, _path: &Path, _fh: Option<u64>, _mode: u32) -> ResultEmpty {
+        Err(libc::EROFS)
+    }
+
+    fn chown(
+        &self,
+        _req: RequestInfo,
+        _path: &Path,
+        _fh: Option<u64>,
+        _uid: Option<u32>,
+        _gid: Option<u32>,
+    ) -> ResultEmpty {
+        Err(libc::EROFS)
+    }
+
+    fn truncate(
+        &self,
+        _req: RequestInfo,
+        _path: &Path,
+        _fh: Option<u64>,
+        _size: u64,
+    ) -> ResultEmpty {
+        Err(libc::EROFS)
+    }
+
+    fn utimens(
+        &self,
+        _req: RequestInfo,
+        _path: &Path,
+        _fh: Option<u64>,
+        _atime: Option<Timespec>,
+        _mtime: Option<Timespec>,
+    ) -> ResultEmpty {
+        Err(libc::ENOSYS)
+    }
+
+    fn utimens_macos(
+        &self,
+        _req: RequestInfo,
+        _path: &Path,
+        _fh: Option<u64>,
+        _crtime: Option<Timespec>,
+        _chgtime: Option<Timespec>,
+        _bkuptime: Option<Timespec>,
+        _flags: Option<u32>,
+    ) -> ResultEmpty {
+        Err(libc::ENOSYS)
+    }
+
+    fn readlink(&self, _req: RequestInfo, _path: &Path) -> ResultData {
+        Err(libc::ENOSYS)
+    }
+
+    fn mknod(
+        &self,
+        _req: RequestInfo,
+        _parent: &Path,
+        _name: &OsStr,
+        _mode: u32,
+        _rdev: u32,
+    ) -> ResultEntry {
+        Err(libc::EROFS)
+    }
+
+    fn mkdir(&self, _req: RequestInfo, _parent: &Path, _name: &OsStr, _mode: u32) -> ResultEntry {
+        Err(libc::EROFS)
+    }
+
+    fn unlink(&self, _req: RequestInfo, _parent: &Path, _name: &OsStr) -> ResultEmpty {
+        Err(libc::EROFS)
+    }
+
+    fn rmdir(&self, _req: RequestInfo, _parent: &Path, _name: &OsStr) -> ResultEmpty {
+        Err(libc::EROFS)
+    }
+
+    fn symlink(
+        &self,
+        _req: RequestInfo,
+        _parent: &Path,
+        _name: &OsStr,
+        _target: &Path,
+    ) -> ResultEntry {
+        Err(libc::EROFS)
+    }
+
+    fn rename(
+        &self,
+        _req: RequestInfo,
+        _parent: &Path,
+        _name: &OsStr,
+        _newparent: &Path,
+        _newname: &OsStr,
+    ) -> ResultEmpty {
+        Err(libc::EROFS)
+    }
+
+    fn link(
+        &self,
+        _req: RequestInfo,
+        _path: &Path,
+        _newparent: &Path,
+        _newname: &OsStr,
+    ) -> ResultEntry {
+        Err(libc::EROFS)
+    }
+
+    fn open(&self, _req: RequestInfo, _path: &Path, _flags: u32) -> ResultOpen {
+        Err(libc::ENOSYS)
+    }
+
+    fn read(
+        &self,
+        _req: RequestInfo,
+        _path: &Path,
+        _fh: u64,
+        _offset: u64,
+        _size: u32,
+        callback: impl FnOnce(ResultSlice<'_>) -> CallbackResult,
+    ) -> CallbackResult {
+        callback(Err(libc::ENOSYS))
+    }
+
+    fn write(
+        &self,
+        _req: RequestInfo,
+        _path: &Path,
+        _fh: u64,
+        _offset: u64,
+        _data: Vec<u8>,
+        _flags: u32,
+    ) -> ResultWrite {
+        Err(libc::EROFS)
+    }
+
+    fn flush(&self, _req: RequestInfo, _path: &Path, _fh: u64, _lock_owner: u64) -> ResultEmpty {
+        Err(libc::EROFS)
+    }
+
+    fn release(
+        &self,
+        _req: RequestInfo,
+        _path: &Path,
+        _fh: u64,
+        _flags: u32,
+        _lock_owner: u64,
+        _flush: bool,
+    ) -> ResultEmpty {
+        Err(libc::ENOSYS)
+    }
+
+    fn fsync(&self, _req: RequestInfo, _path: &Path, _fh: u64, _datasync: bool) -> ResultEmpty {
+        Err(libc::EROFS)
     }
 
     fn opendir(&self, _req: RequestInfo, path: &Path, _flags: u32) -> ResultOpen {
@@ -145,5 +304,52 @@ impl FilesystemMT for Ext4FS {
         } else {
             Err(libc::EBADF)
         }
+    }
+
+    fn fsyncdir(&self, _req: RequestInfo, _path: &Path, _fh: u64, _datasync: bool) -> ResultEmpty {
+        Err(libc::EROFS)
+    }
+
+    fn statfs(&self, _req: RequestInfo, _path: &Path) -> ResultStatfs {
+        Err(libc::ENOSYS)
+    }
+
+    fn setxattr(
+        &self,
+        _req: RequestInfo,
+        _path: &Path,
+        _name: &OsStr,
+        _value: &[u8],
+        _flags: u32,
+        _position: u32,
+    ) -> ResultEmpty {
+        Err(libc::EROFS)
+    }
+
+    fn getxattr(&self, _req: RequestInfo, _path: &Path, _name: &OsStr, _size: u32) -> ResultXattr {
+        Err(libc::ENOSYS)
+    }
+
+    fn listxattr(&self, _req: RequestInfo, _path: &Path, _size: u32) -> ResultXattr {
+        Err(libc::ENOSYS)
+    }
+
+    fn removexattr(&self, _req: RequestInfo, _path: &Path, _name: &OsStr) -> ResultEmpty {
+        Err(libc::EROFS)
+    }
+
+    fn access(&self, _req: RequestInfo, _path: &Path, _mask: u32) -> ResultEmpty {
+        Err(libc::ENOSYS)
+    }
+
+    fn create(
+        &self,
+        _req: RequestInfo,
+        _parent: &Path,
+        _name: &OsStr,
+        _mode: u32,
+        _flags: u32,
+    ) -> ResultCreate {
+        Err(libc::EROFS)
     }
 }
